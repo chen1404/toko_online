f570e14eb80fda0a4a19f17a1b665417ec614276
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-11-12 03:28:47 +0800

    Keranjang, Transaksi, Image Upload

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -132,7 +132,7 @@
             </div>
           </div>
           <div class="card shadow" style="width: 18rem;" onclick="window.location='#'">
-            <img src="img/logo/penjual.png" class="card-img-top" alt="..." a href="#>
+            <img src="img/logo/penjual.png" class="card-img-top" alt="..." a href="#">
             <a href=""></a>
             <div class="card-body">
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
