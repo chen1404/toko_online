f695c39f7ffcb656741b0e80a7c028ee2d1420d2
Satria Bagus Eka Candra <satriabaguse@gmail.com>
2022-11-14 14:53:48 +0800

    nah

--- resources/views/pembeli/keranjang.blade.php
+++ resources/views/pembeli/keranjang.blade.php
@@ -1,39 +1,286 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 
-@section('title')
-  Penjual Homepage
-@endsection
+    <title>Weesia - Your Best Marketplace</title>
 
-@section('content')
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link href="{{ asset('style/main.css') }}" rel="stylesheet" />
+    <link href="{{ asset('stylesheet/style.css') }}" rel="stylesheet" />
+  </head>
 
-<div class="container mt-5 pt-5">
-    @if(session('success'))
-        <div class="">
-            <p>{{ session('success') }}</p>
+  <body>
+<nav
+      class="navbar navbar-expand-lg navbar-light navbar-store fixed-top navbar-fixed-top"
+      data-aos="fade-down"
+    >
+      <div class="container">
+        <a href="{{ route( Auth::user() == 'penjual' ? 'penjual.home' : 'pembeli.home') }}" class="navbar-brand">
+          <img src="{{ asset('images/logo.svg') }}" alt="logo" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item ">
+              <a href="{{ route( Auth::user() == 'penjual' ? 'penjual.home' : 'pembeli.home') }}" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item">
+              <a href="#" class="nav-link">Categories</a>
+            </li>
+        
+            @if(Auth::user())
+              @if(Auth::user()->role == 'pembeli')
+                <li class="nav-item">
+                  <a href="{{ route('pembeli.checkout') }}" class="nav-link" aria-current="page">Profil</a>
+                </li>
+                <li class="nav-item">
+                  <a href="{{ route('pembeli.keranjang') }}" class="nav-link" aria-current="page">Keranjang</a>
+                </li>
+              @endif
+            @else
+              <li class="nav-item">
+                <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
+              </li>
+            @endif
+            <li class="nav-item">
+              @php $stat = Auth::user() ? 'logout' : 'login' @endphp
+              <a class="btn btn-danger" href="{{ "/$stat" }}" class="nav-link " aria-current="page">
+                  {{ ucfirst($stat) }}
+              </a>
+            </li>
+          </ul>
         </div>
-    @endif
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">No</th>
-            <th scope="col">Nama</th>
-            <th scope="col">Harga</th>
-            <th scope="col">Pembeli Id</th>
-        </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            @php $no = 1 @endphp
-            @foreach($keranjangs as $keranjang)
-                <tr>
-                    <th scope="row">{{ $no++ }}</th>
-                    <td>{{ $keranjang->produk->nama }}</td>
-                    <td>{{ $keranjang->harga }}</td>
-                    <td>{{ $keranjang->pembeli_id }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <a href="{{ route('checkout', Auth::user()->id) }}" class="btn btn-warning">Checkout</a>
-</div>
+      </div>
+    </nav>
 
-@endsection
+    <!-- Page Content -->
+    <div class="page-content page-cart">
+      <section
+        class="store-breadcrumbs"
+        data-aos="fade-down"
+        data-aos-delay="100"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <nav>
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item">
+                    <a href="/index.html">Home</a>
+                  </li>
+                  <li class="breadcrumb-item active">Cart</li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section class="store-cart">
+        <div class="container">
+            @if(session('success'))
+            <div class="">
+                <p>{{ session('success') }}</p>
+            </div>
+            @endif
+
+          <div class="row" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-12 table-responsive">
+              <table class="table table-borderless table-cart">
+                <thead>
+                  <tr>
+                    <td>Image</td>
+                    <td>Name &amp; Seller</td>
+                    <td>Price</td>
+                    <td>Menu</td>
+                  </tr>
+                </thead>
+                <tbody>
+                @php $no = 1 @endphp
+                @foreach($keranjangs as $keranjang)
+                  <tr>
+                    <td style="width: 20%">
+                      <img
+                        src="/images/product-cart-1.png"
+                        alt=""
+                        class="cart-image w-100"
+                      />
+                    </td>
+                    <td td style="width: 35%">
+                      <div class="product-title">{{ $keranjang->produk->nama }}</div>
+                      <div class="product-subtitle">{{ $keranjang->pembeli_id    }}</div>
+                    </td>
+                    <td td style="width: 35%">
+                      <div class="product-title">Rp.{{ $keranjang->harga }}</div>
+                      <div class="product-subtitle">IDR</div>
+                    </td>
+                    <td style="width: 20%">
+                      <a href="#" class="btn btn-remove-cart"> remove </a>
+                    </td>
+                  </tr>
+                </tbody>
+                @endforeach
+              </table>
+            </div>
+          </div>
+          <div class="row" data-aos="fade-up" data-aos-delay="150">
+            <div class="col-12">
+              <hr />
+            </div>
+            <div class="col-12">
+              <h2 class="mb-4">Shipping Details</h2>
+            </div>
+          </div>
+          <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="addressOne">Address 1</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="addressOne"
+                  name="addressOne"
+                  value="Setra Duta Cemara"
+                />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="addressTwo">Address 2</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="addressOne"
+                  name="addressOne"
+                  value="Blok B2 No. 34"
+                />
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="Province">Province</label>
+                <select name="Province" id="Province" class="form-control">
+                  <option value="West Java">West Java</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="Province">city</label>
+                <select name="city" id="city" class="form-control">
+                  <option value="Bandung">Bandung</option>
+                </select>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="form-group">
+                <label for="postalCode">Postal Code</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="postalCode"
+                  name="postalCode"
+                  value="40512"
+                />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="country">Country</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="country"
+                  name="country"
+                  value="Indonesia "
+                />
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="form-group">
+                <label for="mobile">Mobile</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="mobile"
+                  name="mobile"
+                  value="+628 2020 11111"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="row" data-aos="fade-up" data-aos-delay="150">
+            <div class="col-12">
+              <hr />
+            </div>
+            <div class="col-12">
+              <h2 class="mb-1">Payment Information</h2>
+            </div>
+          </div>
+          <div class="row" data-aos="fade-up" data-aos-delay="200">
+            <div class="col-4 col-md-2">
+              <div class="product-title">$10</div>
+              <div class="product-subtitle">Country Tax</div>
+            </div>
+            <div class="col-4 col-md-3">
+              <div class="product-title">$10</div>
+              <div class="product-subtitle">Product Insurance</div>
+            </div>
+            <div class="col-4 col-md-2">
+              <div class="product-title">$580</div>
+              <div class="product-subtitle">Ship to Jakarta</div>
+            </div>
+            <div class="col-4 col-md-2">
+              <div class="product-title text-success">$392,409</div>
+              <div class="product-subtitle">Total</div>
+            </div>
+            <div class="col-8 col-md-3">
+              <a
+                href="/success.html"
+                class="btn btn-success mt-4 px-4 btn-block"
+                >Checkout Now</a
+              >
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2">2022 Copyright Weesia. All Rights Reserved</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.js"
+      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+      crossorigin="anonymous"
+    ></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
+    <script src="/script/navbar-scroll.js"></script>
+  </body>
+</html>
