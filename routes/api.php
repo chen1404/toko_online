08c1321458a8139f949aba1d66885d9be0c0da23
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-11-18 03:48:13 +0800

    API anjay

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\ProdukController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/produk', [ProdukController::class, 'getProduk']);
+
+Route::get('/produk/{id}', [ProdukController::class, 'produk']);
+
+Route::post('/produk/create', [ProdukController::class, 'createProduk']);
