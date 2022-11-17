5a91b70042df990ccb78864b3007d063a7606d5f
Satria Bagus Eka Candra <satriabaguse@gmail.com>
2022-11-17 23:00:44 +0800

    pagination

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -11,19 +11,20 @@ use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $provinsi = $request->get('Province');
         $kota = $request->get('city');
         $alamat = $request->get('addressOne');
         $nohp = $request->get('mobile');
         $jumlahBarang = $request->get('counts');
 
-        if($jumlahBarang > 0) {
+        if ($jumlahBarang > 0) {
             $id = Auth::user()->id;
             $carts = Keranjang::all()->where('pembeli_id', $id);
-            $alamatLengkap = $provinsi.', '.$kota.'. '.$alamat.'; '.$nohp;
+            $alamatLengkap = $provinsi . ', ' . $kota . '. ' . $alamat . '; ' . $nohp;
 
-            foreach($carts as $cart) {
+            foreach ($carts as $cart) {
                 $produk = Produk::findOrFail($cart->produk->id);
 
                 $sisaBarang = $produk->stok - $cart->jumlah_barang;
@@ -36,7 +37,7 @@ class TransaksiController extends Controller
                     "pembeli_id" => $id,
                     "penjual_id" => $cart->produk->penjual_id,
                     "produk_id" => $cart->produk->id,
-                ]); 
+                ]);
                 $transaksi->save();
             }
             Keranjang::where('pembeli_id', $id)->delete();
@@ -45,12 +46,13 @@ class TransaksiController extends Controller
             return redirect('/keranjang');
         }
     }
-    
-    public function storeProduk(Produk $product) {
+
+    public function storeProduk(Produk $product)
+    {
         $sisaBarang = $product->stok - 1;
-        if($sisaBarang >= 0) {
+        if ($sisaBarang >= 0) {
             $product->update(['stok' => $sisaBarang]);
-            
+
             $transaksi = new Transaksi([
                 "total_harga" => $product->harga,
                 "jumlah_barang" => '1',
@@ -58,23 +60,24 @@ class TransaksiController extends Controller
                 "pembeli_id" => Auth::user()->id,
                 "penjual_id" => $product->penjual_id,
                 "produk_id" => $product->id,
-            ]); 
+            ]);
             $transaksi->save();
-            
+
             return redirect("/show/$product->id")->with('success', 'Produk berhasil di Checkout!');
         } else {
             return redirect("/show/$product->id");
         }
     }
 
-    public function daftarTransaksi() {
+    public function daftarTransaksi()
+    {
         $id = Auth::user()->id;
         $transaksi = Transaksi::all()->where('pembeli_id', $id);
         $user = User::all()->where('id', $id)->first();
         $totalBarang = Transaksi::all()->where('pembeli_id', $id)->sum('jumlah_barang');
 
         $totalPengeluaran = 0;
-        foreach($transaksi as $trans) {
+        foreach ($transaksi as $trans) {
             $totalPengeluaran += $trans->total_harga;
         }
 
@@ -86,12 +89,13 @@ class TransaksiController extends Controller
         ]);
     }
 
-    public function penjual() {
+    public function penjual()
+    {
         $transactions = Transaksi::all()->where('penjual_id', Auth::user()->id);
         $jumlahTransaksi = Transaksi::all()->where('penjual_id', Auth::user()->id)->count();
         $totalIncome = Transaksi::where('penjual_id', Auth::user()->id)->sum('total_harga');
         $jumlahCustomer = Transaksi::select('pembeli_id')->where('penjual_id', Auth::user()->id)->distinct()->get()->count();
-        
+
         return view('penjual.home', [
             "transactions" => $transactions,
             "jumlah_transaksi" => $jumlahTransaksi,
