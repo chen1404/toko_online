965054c44088157a373709f19e3bd97e89e4fedc
rosyanxone <rausyanfikrkarmayoga@gmail.com>
2022-11-18 14:40:38 +0800

    last commit

--- resources/views/pembeli/home.blade.php
+++ resources/views/pembeli/home.blade.php
@@ -115,20 +115,6 @@
                         </div>
                     @endforeach
                 </div>
-
-                {{-- <nav aria-label="Page navigation example">
-                    <ul class="pagination justify-content-end">
-                        <li class="page-item disabled">
-                            <a class="page-link">Previous</a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $products->links() }}">Next</a>
-                        </li>
-                    </ul>
-                </nav> --}}
                 @if($products->hasPages())
                     <div class="">
                         {{ $products->links() }}
--- resources/views/penjual/home.blade.php
+++ resources/views/penjual/home.blade.php
@@ -158,19 +158,6 @@
                                     {{ $transactions->links() }}
                                 </div>
                             @endif
-                            {{-- <nav aria-label="Page navigation example">
-                                <ul class="pagination justify-content-end">
-                                    <li class="page-item disabled">
-                                        <a class="page-link">Previous</a>
-                                    </li>
-                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="#">Next</a>
-                                    </li>
-                                </ul>
-                            </nav> --}}
                         </div>
                     </div>
                 </div>
